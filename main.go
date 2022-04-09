ca70042cc4f0b096f84ce78452585459887f1024
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-04-10 01:42:19 +0800

    :sparkles: create blocks as a chain

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("hello cyain --a block chain demo.")
+	bc := NewBlockchain()
+	
+	bc.AddBlock("Send 1 BTC to me")
+	bc.AddBlock("Send 2 more BTC to you")
+	
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
