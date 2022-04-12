ea5099e73e57c3ba91c6c40bfac02150b77a9fed
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-04-13 01:09:58 +0800

    :sparkles: add persistence

--- block.go
+++ block.go
@@ -3,8 +3,16 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
 	"strconv"
 	"time"
+	
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbFile       = "blockchain.db"
+	blocksBucket = "blocks"
 )
 
 type Block struct {
@@ -45,13 +53,34 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 }
 
 type BlockChain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 func (bc *BlockChain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	var lastHash []byte
+	
+	viewf := func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+		return nil
+	}
+	err := bc.db.View(viewf)
+	
+	newBlock := NewBlock(data, lastHash)
+	
+	updatef := func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.SerializeBlock())
+		err = b.Put([]byte("l"), newBlock.Hash)
+		bc.tip = newBlock.Hash
+		
+		_ = err
+		return nil
+	}
+	
+	_ = err
+	err = bc.db.Update(updatef)
 }
 
 func NewGenesisBlock() *Block {
@@ -59,9 +88,81 @@ func NewGenesisBlock() *Block {
 }
 
 func NewBlockchain() *BlockChain {
-	return &BlockChain{
-		[]*Block{
-			NewGenesisBlock(),
-		},
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+	
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, _ := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.SerializeBlock())
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+		
+		return nil
+	})
+	
+	bc := BlockChain{
+		tip,
+		db,
 	}
+	
+	_ = err
+	return &bc
+}
+
+func (b *Block) SerializeBlock() []byte {
+	var result bytes.Buffer
+	
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	_ = err
+	
+	return result.Bytes()
+}
+
+func DeserializeBlock(data []byte) *Block {
+	var block Block
+	
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+	_ = err
+	
+	return &block
+}
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db *bolt.DB
+}
+
+func (bc *BlockChain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{
+		bc.tip,
+		bc.db,
+	}
+	
+	return bci
+}
+
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+	
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+		
+		return nil
+	})
+	
+	i.currentHash = block.PrevBlockHash
+	
+	_ = err
+	return block
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,6 @@ module cyain
 
 go 1.17
 
+require github.com/boltdb/bolt v1.3.1
+
+require golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
