f9d047b2a8b294ecdc7865f5cef2ac16f41f9ba4
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-04-21 23:59:54 +0800

    :sparkles:

---
+++ wallet.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+)
+
+const (
+	version            = byte(0x00)
+	addressChecksumLen = 4
+)
+
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+	wallet := Wallet{
+		private,
+		public,
+	}
+
+	return &wallet
+}
+
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+	private, _ := ecdsa.GenerateKey(curve, rand.Reader)
+	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+
+	return *private, pubKey
+}
+
+func (w Wallet) GetAddress() []byte {
+	pubKeyHash := HashPubKey(w.PublicKey)
+
+	versionedPayload := append([]byte{version}, pubKeyHash...)
+	checksum := checksum(versionedPayload)
+
+	fullPayload := append(versionedPayload, checksum...)
+	address := Base58Encode(fullPayload)
+
+	return address
+}
+
+func HashPubKey(pubkey []byte) []byte {
+	public := sha256.Sum256(pubkey)
+	RIPEMD160Hasher := ripemd160.New()
+	_, _ = RIPEMD160Hasher.Write(public)
+	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
+	return publicRIPEMD160
+}
+
+func checksum(payload []byte) []byte {
+	first := sha256.Sum256(payload)
+	second := sha256.Sum256(first[:addressChecksumLen])
+
+	return second[:]
+}
